154cb09da262adc963e0732b8e7e08c3fe386400
vibe <vitaliy.beletskiy@gmail.com>
2025-02-24 14:29:31 +0100

    add a comment to the fix in GetProductsUseCase;

--- app/src/main/java/vibe/shopper/domain/GetProductsUseCase.kt
+++ app/src/main/java/vibe/shopper/domain/GetProductsUseCase.kt
@@ -2,7 +2,6 @@ package vibe.shopper.domain
 
 import vibe.shopper.data.ProductRepository
 import vibe.shopper.data.model.Chair
-import vibe.shopper.data.model.Couch
 import vibe.shopper.data.model.Failure
 import vibe.shopper.data.model.Product
 import vibe.shopper.data.model.Result
@@ -14,21 +13,21 @@ import javax.inject.Inject
 class GetProductsUseCase @Inject constructor(
     private val productRepository: ProductRepository,
 ) {
+    // FIXME: quick-and-dirty patch to display at least some images. For testing purposes only.
     suspend fun getProducts(): Result<List<Product>, Exception> =
         productRepository.getApiProducts().fold(
             ifSuccess = {
+                // Success(it.products)
                 val products = it.products.map { product ->
                     val url =
                         "https://www.ikea.com/se/sv/images/products/oestanoe-stol-svart-remmarn-moerkgra__1119282_pe873451_s5.jpg?f=xs"
                     if (product is Chair) {
                         product.copy(imageUrl = url)
                     } else {
-//                        (product as Couch).copy(imageUrl = url)
                         product
                     }
                 }
                 Success(products)
-//                Success(it.products)
             },
             ifFailure = {
                 Failure(it)
